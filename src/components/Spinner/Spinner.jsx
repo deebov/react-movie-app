e5b3732331ca544c811f5d56d128d22636cc0600
Dilshod Turobov <deebov@yandex.com>
2019-01-06 02:09:54 +0500

    first commit

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Spinner.module.css';
+
+export const Spinner = ({ type = 'big', className = '' }) => {
+  return (
+    <div className={styles.Container}>
+      <span className={[styles.Spinner, styles[type], className].join(' ')} />
+    </div>
+  );
+};
+
+Spinner.propTypes = {
+  type: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Spinner.defaultProps = {
+  type: 'big',
+  className: '',
+};
+
+export default Spinner;
