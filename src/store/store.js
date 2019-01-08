12336827177e4340229cfd7d425727fc343390b9
Dilshod Turobov <deebov@yandex.com>
2019-01-08 14:22:16 +0500

    ONLY use redux devtools in development mode

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-const composeEnhancers = composeWithDevTools({});
+const composeEnhancers =
+  process.env.MODE === 'development' ? composeWithDevTools({}) : compose;
 
 const store = createStore(
   rootReducer,
